582cb41851a08135c71ad8b68789d0601d5b8309
DaniyalSKKR <122335626+DaniyalSKKR@users.noreply.github.com>
2024-05-20 20:19:47 -0500

    Update main.fs

--- main.fs
+++ main.fs
@@ -3,12 +3,11 @@
 // function denoting the compiler, calling the lexer, 
 // parser, etc.
 //
-// <<YOUR NAME>>
+// Daniyal Khokhar
 //
 // Original author:
 //   Prof. Joe Hummel
-//   U. of Illinois, Chicago
-//   CS 341, Spring 2022
+//   Spring 2022
 //
 
 
